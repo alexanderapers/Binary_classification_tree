bfbeb089b7f359e4ddaddb7c953907b840a860e6
alexanderapers <alexander@apers.nl>
2023-11-08 21:10:23 +0100

    removed old datasets

--- load_data.py
+++ load_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from typing import Tuple, Dict
-import csv
 
 def get_columns_to_use():
     prefixes = ["FOUT", "MLOC", "NBD", "PAR", "VG", "NOF", "NOM", "NSF", "NSM", "ACD", "NOI", "NOT", "TLOC"]
@@ -28,19 +27,3 @@ def get_eclipse_dataset(version: int) -> Tuple[np.ndarray, np.ndarray, Dict[int,
     y = (df["post"] > 0).to_numpy(dtype=np.bool8)
     return x, y, feature_names
 
-
-def get_pima_dataset() -> Tuple[np.ndarray, np.ndarray]:
-    df = np.genfromtxt("data/pima.csv", delimiter=",")
-    x = df[:, :-1]
-    y = df[:, -1]
-    return x, y
-
-
-def get_credit_dataset() -> Tuple[np.ndarray, np.ndarray, Dict[int, str]]:
-    with open("data/credit.csv", "r") as csvfile:
-        names = next(csv.reader(csvfile))
-        feature_names = {k:v for k, v in enumerate(names[:-1])}
-    df = np.genfromtxt("credit.csv", delimiter=",", skip_header=True)
-    x = df[:, :-1]
-    y = df[:, -1]
-    return x, y, feature_names
